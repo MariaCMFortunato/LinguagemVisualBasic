624184a6f974aec29b7ce9140824361169b619f8
Maria <conceicao.martinho.estudo@gmail.com>
2023-10-10 13:25:12 +0100

    Atualização

--- 3934ProgramacaoEmVisualBasic/Exercicio04/Form1.Designer.vb
+++ 3934ProgramacaoEmVisualBasic/Exercicio04/Form1.Designer.vb
@@ -24,6 +24,11 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Label1 = New Label()
         Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Button1 = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -33,26 +38,81 @@ Partial Class Form1
         Label1.ForeColor = SystemColors.HotTrack
         Label1.Location = New Point(28, 27)
         Label1.Name = "Label1"
-        Label1.Size = New Size(636, 30)
+        Label1.Size = New Size(643, 30)
         Label1.TabIndex = 0
-        Label1.Text = "Exemplo De declaração de variáveis - Integer (Números  Inteiros"
+        Label1.Text = "Exemplo De declaração de variáveis - Integer (Números  Inteiros)"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point)
         Label2.ForeColor = SystemColors.Highlight
-        Label2.Location = New Point(44, 73)
+        Label2.Location = New Point(319, 97)
         Label2.Name = "Label2"
-        Label2.Size = New Size(76, 30)
+        Label2.Size = New Size(0, 30)
         Label2.TabIndex = 1
-        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.ForeColor = SystemColors.Highlight
+        Label3.Location = New Point(39, 97)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 30)
+        Label3.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.ForeColor = SystemColors.Highlight
+        Label4.Location = New Point(121, 97)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 30)
+        Label4.TabIndex = 3
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.ForeColor = SystemColors.Highlight
+        Label5.Location = New Point(203, 97)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 30)
+        Label5.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.ForeColor = SystemColors.Highlight
+        Label6.Location = New Point(285, 97)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(28, 30)
+        Label6.TabIndex = 5
+        Label6.Text = "="
+        Label6.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(63, 164)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(119, 42)
+        Button1.TabIndex = 6
+        Button1.Text = "Testar variáveis"
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(685, 450)
+        ClientSize = New Size(685, 266)
+        Controls.Add(Button1)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(Label1)
         Name = "Form1"
@@ -63,4 +123,9 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- 3934ProgramacaoEmVisualBasic/Exercicio04/Form1.vb
+++ 3934ProgramacaoEmVisualBasic/Exercicio04/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'variavel tipo Integer
         Dim a As Integer
         Dim b As Integer
@@ -14,6 +19,9 @@
         '& concatenação de variável numérica
 
         Label2.Text = c
-
+        Label3.Text = a
+        Label4.Text = " + "
+        Label5.Text = b
     End Sub
+
 End Class
